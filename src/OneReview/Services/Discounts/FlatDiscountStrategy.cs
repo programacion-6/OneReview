1ff4045674dc0614ab6cc467daff3bcf14ac04ce
luiggy-mamani-jalafund <luiggy.mamani.12777190@usalesiana.edu.bo>
2024-09-06 21:44:37 -0400

    refactor: discount strategies moved to a different folder

---
+++ src/OneReview/Services/Discounts/FlatDiscountStrategy.cs
@@ -0,0 +1,12 @@
+﻿namespace OneReview.Services;
+
+public class FlatDiscountStrategy : IDiscountStrategy
+{
+    // class DiscountConstants
+    private const decimal DeafultAmountDiscount = 50;
+
+    public decimal ApplyDiscount(decimal totalAmount)
+    {
+        return totalAmount - DeafultAmountDiscount;
+    }
+}
