7334fabf26d098ef6c7d5c413726ce6db567fc7c
gabygabylagaby <gabrielagv7656@gmail.com>
2024-09-07 05:44:24 -0400

    feat: Apply strategy in Payment

---
+++ src/OneReview/Services/Payment/StripePaymentStrategy.cs
@@ -0,0 +1,8 @@
+namespace OneReview.Services;
+public class StripePaymentStrategy : IPaymentStrategy
+{
+    public void ProcessPayment(decimal amount)
+    {
+        Console.WriteLine($"Processing {amount} payment through Stripe");
+    }
+}
