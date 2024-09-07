fae5533bf700cd0402550ba0c4994f1e14685103
José Luis Terán Rocha <jose.luis.teran.rocha.2@gmail.com>
2024-09-07 02:45:22 -0400

    feat: apply tax process to orders

--- src/OneReview/Services/OrderService.cs
+++ src/OneReview/Services/OrderService.cs
@@ -1,4 +1,6 @@
-﻿using OneReview.Services.Concretes.Payment;
+﻿using OneReview.Services.Composites;
+using OneReview.Services.Concretes.Payment;
+using OneReview.Services.Concretes.Taxes;
 using OneReview.Services.Contexts;
 using OneReview.Services.Interfaces;
 
@@ -27,14 +29,26 @@ public class OrderService(IDiscountStrategy discountStrategy)
         // apply discount
         totalAmount = _discountStrategy.ApplyDiscount(totalAmount);
 
-        // process payment
+        // apply taxes
+        var localTax = new LocalTax(0.45m);
+        var stateTax = new StateTax(0.27m);
+        var federalTax = new FederalTax(0.12m);
+
+        var compositeTax = new CompositeTax();
+        compositeTax.Add(localTax);
+        compositeTax.Add(stateTax);
+        compositeTax.Add(federalTax);
+
+        decimal totalTax = compositeTax.CalculateTax(totalAmount);
+
+        // payment process
         var bankTransferContext = new PaymentContext(new BankTransferStrategy());
-        bankTransferContext.PaymentExecution(totalAmount);
+        bankTransferContext.PaymentExecution(totalAmount + totalTax);
 
         var payPalContext = new PaymentContext(new PayPalStrategy());
-        payPalContext.PaymentExecution(totalAmount);
+        payPalContext.PaymentExecution(totalAmount + totalTax);
 
         var stripeContext = new PaymentContext(new StripeStrategy());
-        stripeContext.PaymentExecution(totalAmount);
+        stripeContext.PaymentExecution(totalAmount + totalTax);
     }
 }
