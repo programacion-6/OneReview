bf13221642f9dd3441ab3569f6f5cada7e0eb37e
salet-gutierrez-jalafund <salet.gutierrez@fundacion-jala.org>
2024-09-10 00:33:00 -0400

    refactor: remove IDiscountStrategy interface injections in classes

--- src/OneReview/OverServices/PayPalPaymentService.cs
+++ src/OneReview/OverServices/PayPalPaymentService.cs
@@ -1,19 +1,9 @@
-﻿using OneReview.OverServices.Discounts;
-
-namespace OneReview.OverServices;
+﻿namespace OneReview.OverServices;
 
 public class PayPalPaymentService : IPaymentService
 {
-    private readonly IDiscountStrategy _discountStrategy;
-
-    public PayPalPaymentService(IDiscountStrategy discountStrategy)
-    {
-        _discountStrategy = discountStrategy;
-    }
-
     public void ProcessPayment(decimal amount)
     {
-        var discountAmount = _discountStrategy.ApplyDiscount(amount);
-        Console.WriteLine($"Processing {discountAmount} through PayPal with discount.");
+        Console.WriteLine($"Processing {amount} through PayPal with discount.");
     }
 }
--- src/OneReview/OverServices/StripePaymentService.cs
+++ src/OneReview/OverServices/StripePaymentService.cs
@@ -1,19 +1,10 @@
-﻿using OneReview.OverServices.Discounts;
-
-namespace OneReview.OverServices;
+﻿namespace OneReview.OverServices;
 
 public class StripePaymentService : IPaymentService
 {
-    private readonly IDiscountStrategy _discountStrategy;
-
-    public StripePaymentService(IDiscountStrategy discountStrategy)
-    {
-        _discountStrategy = discountStrategy;
-    }
 
     public void ProcessPayment(decimal amount)
     {
-        var discountAmount = _discountStrategy.ApplyDiscount(amount);
-        Console.WriteLine($"Processing {discountAmount} through Stripe with discount.");
+        Console.WriteLine($"Processing {amount} through Stripe with discount.");
     }
 }
