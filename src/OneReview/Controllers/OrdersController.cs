1424dd69f798ab4675e07b18b14b0b23661cff25
Alberth Condori <alberth.condori@fundacion-jala.org>
2024-09-10 09:18:21 -0400

    refactor: refactor services behavior, decouple code

---
+++ src/OneReview/Controllers/OrdersController.cs
@@ -0,0 +1,34 @@
+﻿namespace OneReview.Controllers;
+
+using Microsoft.AspNetCore.Mvc;
+using OneReview.OverServices;
+using OneReview.OverServices.Discounts;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class OrdersController(OrderService orderService) : ControllerBase
+{
+    private readonly OrderService _orderService = orderService;
+
+    /// <summary>
+    /// 
+    /// </summary>
+    /// <param name="totalAmount">This should be a model.</param>
+    /// <param name="discountType">You should parse this</param>
+    /// <returns></returns>
+    [HttpPost("paypal")]
+    public IActionResult ProcessOrderPaypal([FromBody] decimal totalAmount, [FromQuery] DiscountType discountType)
+    {
+        _orderService.ProcessOrder(totalAmount, discountType, PaymentMethod.Paypal);
+
+        return Ok("Order processed successfully.");
+    }
+
+    [HttpPost("stripe")]
+    public IActionResult ProcessOrderStripe([FromBody] decimal totalAmount, [FromQuery] DiscountType discountType)
+    {
+        _orderService.ProcessOrder(totalAmount, discountType, PaymentMethod.Stripe);
+
+        return Ok("Order processed successfully.");
+    }
+}
---
+++ src/OneReview/OverServices/OrderService.cs
@@ -0,0 +1,27 @@
+﻿namespace OneReview.OverServices;
+
+using OneReview.OverServices.Discounts;
+
+public class OrderService
+{
+    // Apply discount
+    // Process payment
+    public void ProcessOrder(decimal totalAmount, DiscountType discountType, PaymentMethod paymentMethod)
+    {
+        var discountAmount = ApplyDiscount(totalAmount, discountType);
+        ProcessPayment(discountAmount, paymentMethod);
+    }
+
+    private static decimal ApplyDiscount(decimal totalAmount, DiscountType discountType)
+    {
+        var discountStrategy = DiscountStrategyFactory.CreateDiscountStrategy(discountType);
+
+        return discountStrategy.ApplyDiscount(totalAmount);
+    }
+    
+    private static void ProcessPayment(decimal discountAmount, PaymentMethod paymentMethod)
+    {
+        var paymentService = PaymentServiceFactory.CreatePaymentService(paymentMethod);
+        paymentService.ProcessPayment(discountAmount);
+    }
+}
