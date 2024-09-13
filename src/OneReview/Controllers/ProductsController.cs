772f6cb1c9a505d023c9e94b58df9856041fd26a
José Luis Terán Rocha <jose.luis.teran.rocha.2@gmail.com>
2024-09-13 07:25:22 -0400

    refactor: update controller logic

--- src/OneReview/Controllers/ProductsController.cs
+++ src/OneReview/Controllers/ProductsController.cs
@@ -1,43 +1,27 @@
-﻿namespace OneReview.Controllers;
+﻿﻿namespace OneReview.Controllers;
 
 using Microsoft.AspNetCore.Mvc;
+
+using OneReview.Domain;
+
 using OneReview.Mappers.Requests;
 using OneReview.Mappers.Responses;
 using OneReview.Services;
 
 [ApiController]
 [Route("api/v1/[controller]")]
-public class ProductsController(ProductsService productsService) : ControllerBase
+public class ProductsController(ProductsService productsService) 
+    : MainController<CreateProductRequest, ProductResponse, Product>
 {
     private readonly ProductsService _productsService = productsService;
 
-    [HttpPost]
-    public async Task<IActionResult> Create([FromBody] CreateProductRequest request)
+    protected override async Task CreateAsync(Product product)
     {
-        // mapping to internal representation
-        var product = request.ToDomain();
-
-        // invoking the use case
         await _productsService.CreateAsync(product);
-
-        // mapping to external representation
-        var response = ProductResponse.FromDomain(product);
-
-        return CreatedAtAction(
-            actionName: nameof(Get),
-            routeValues: new { ProductId = product.Id },
-            value: response
-        );
     }
 
-    [HttpGet("{productId:guid}")]
-    public async Task<IActionResult> Get([FromRoute] Guid productId)
+    protected override async Task<Product> GetAsync(Guid id)
     {
-        // get the product
-        var product = await _productsService.GetAsync(productId);
-
-        return product is null
-            ? Problem(statusCode: StatusCodes.Status404NotFound, detail: $"Product not found (productId {productId})")
-            : Ok(ProductResponse.FromDomain(product));
+        return await _productsService.GetAsync(id);
     }
 }
