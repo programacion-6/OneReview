88770d8d1eadd8c71b192bafd0e39d017b8f5081
Alex Paca <fujorita772@gmail.com>
2024-09-13 00:49:55 -0400

    refactor: better post method adding base controller

--- src/OneReview/Controllers/ProductsController.cs
+++ src/OneReview/Controllers/ProductsController.cs
@@ -7,37 +7,34 @@ using OneReview.Services;
 
 [ApiController]
 [Route("api/v1/[controller]")]
-public class ProductsController(ProductsService productsService) : ControllerBase
+public class ProductsController : BaseConstroller
 {
-    private readonly ProductsService _productsService = productsService;
+    private readonly ProductsService _productsService;
+
+    public ProductsController(ProductsService productsService)
+    {
+        _productsService = productsService;
+    }
 
     [HttpPost]
     public async Task<IActionResult> Create([FromBody] CreateProductRequest request)
     {
-        // mapping to internal representation
-        var product = request.ToDomain();
+        var validationResult = ValidateRequest(request);
+        if (validationResult != null) return validationResult;
 
-        // invoking the use case
+        var product = request.ToDomain();
         await _productsService.CreateAsync(product);
 
-        // mapping to external representation
-        var response = ProductResponse.FromDomain(product);
-
-        return CreatedAtAction(
-            actionName: nameof(Get),
-            routeValues: new { ProductId = product.Id },
-            value: response
-        );
+        return CreatedAtAction(nameof(Get), new { ProductId = product.Id }, ProductResponse.FromDomain(product));
     }
 
     [HttpGet("{productId:guid}")]
     public async Task<IActionResult> Get([FromRoute] Guid productId)
     {
-        // get the product
         var product = await _productsService.GetAsync(productId);
 
         return product is null
-            ? Problem(statusCode: StatusCodes.Status404NotFound, detail: $"Product not found (productId {productId})")
+            ? HandleNotFound($"Product not found (productId {productId})")
             : Ok(ProductResponse.FromDomain(product));
     }
 }
