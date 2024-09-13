9195ac1f871714a0f7e933b063da518bff09f95d
ignacio-villarroel-jalafund <ignaciovillarroel50@gmail.com>
2024-09-12 21:31:15 -0400

    refactor: separete products controller logic

--- src/OneReview/Controllers/ProductsController.cs
+++ src/OneReview/Controllers/ProductsController.cs
@@ -2,42 +2,33 @@
 
 using Microsoft.AspNetCore.Mvc;
 using OneReview.Mappers.Requests;
-using OneReview.Mappers.Responses;
 using OneReview.Services;
 
 [ApiController]
 [Route("api/v1/[controller]")]
-public class ProductsController(ProductsService productsService) : ControllerBase
+public class ProductsController : ControllerBase
 {
-    private readonly ProductsService _productsService = productsService;
+    private readonly ProductsService _productsService;
+
+    public ProductsController(ProductsService productsService)
+    {
+        _productsService = productsService;
+    }
 
     [HttpPost]
     public async Task<IActionResult> Create([FromBody] CreateProductRequest request)
     {
-        // mapping to internal representation
-        var product = request.ToDomain();
-
-        // invoking the use case
-        await _productsService.CreateAsync(product);
-
-        // mapping to external representation
-        var response = ProductResponse.FromDomain(product);
-
-        return CreatedAtAction(
-            actionName: nameof(Get),
-            routeValues: new { ProductId = product.Id },
-            value: response
-        );
+        var response = await _productsService.CreateProductAsync(request);
+        return CreatedAtAction(nameof(Get), new { ProductId = response.Id }, response);
     }
 
     [HttpGet("{productId:guid}")]
     public async Task<IActionResult> Get([FromRoute] Guid productId)
     {
-        // get the product
-        var product = await _productsService.GetAsync(productId);
+        var response = await _productsService.GetProductByIdAsync(productId);
 
-        return product is null
+        return response is null
             ? Problem(statusCode: StatusCodes.Status404NotFound, detail: $"Product not found (productId {productId})")
-            : Ok(ProductResponse.FromDomain(product));
+            : Ok(response);
     }
 }
